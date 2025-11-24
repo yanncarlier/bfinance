1364ca3f22018efd4dc0e62a16f5fc091f46aab2
Yann <227698+yanncarlier@users.noreply.github.com>
2025-11-24 11:58:30 +0700

    added latency.sh

---
+++ latency.sh
@@ -0,0 +1,9 @@
+curl -o /dev/null -s -w "\
+     namelookup:  %{time_namelookup}s\n\
+        connect:  %{time_connect}s\n\
+     appconnect:  %{time_appconnect}s\n\
+      pretransfer: %{time_pretransfer}s\n\
+         redirect: %{time_redirect}s\n\
+    starttransfer: %{time_starttransfer}s\n\
+         -------------------------\n\
+            total:  %{time_total}s\n"  https://api.binance.com/api/v3/ping
